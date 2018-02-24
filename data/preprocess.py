4687d5067f297fd8e14c1ce272837d90725ee22d
Ayoosh Kathuria <kathuria.ayoosh@gmail.com>
2018-02-24 21:47:47 +0530

    Function to pre-process and clean the data

---
+++ data/preprocess.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt 
+matplotlib.style.use('ggplot')
+from torchnet import meter
+import pickle as pkl 
+
+
+
+
+def preprocess(df):
+    df['bare_nuclei'].replace({'?': np.nan}, inplace = True)
+    df.dropna(inplace=True)
+    df["bare_nuclei"] = df["bare_nuclei"].astype(int)
+    df.drop(["id"], axis = 1, inplace=True)
+    df["class"] = df["class"].map({2:0, 4:1})
+    return df
